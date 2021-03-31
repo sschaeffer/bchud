aa486c00b908be5dffa5c0d411af8e7055c24a6f
sschaeffer <sschaeffer@hotmail.com>
2021-04-01 08:39:15 +0900

    Apr 1st 2021

--- bccurses.py
+++ bccurses.py
@@ -1,15 +1,18 @@
-#!/usr/bin/python3
-
-from datetime import datetime
-from bctimer import BCTimer
-from times import MCTimes
 import curses, time
 from curses import wrapper
 
 def main(stdscr):
+    k = 0
+    cursor_x = 0
+    cursor_y = 0
+
+    # Clear and refresh the screen for a blank canvas
+    stdscr.clear()
+    stdscr.refresh()
 
     curses.halfdelay(5)
     curses.curs_set(0)
+    #clock1 = Clock(stdscr)
 
     # Start colors in curses
     curses.start_color()
@@ -17,45 +20,21 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-    stdscr.clear()
-    stdscr.refresh()
-
-    key = 0
-    cursor_x = 0
-    cursor_y = 0
-
-    bct_time = BCTimer()
-    bct_daytime = BCTimer()
-    mct = MCTimes()
-
-    mcttimestr = ""
-    mctdaytimestr = ""
-    bcttimestr = ""
-    bctdaytimestr = ""
-    bctdaystr = ""
-
     # Loop where k is the last character pressed
-    while (key != ord('q')):
+    while (k != ord('q')):
 
         # Initialization
         stdscr.clear()
         height, width = stdscr.getmaxyx()
 
-        if key == curses.KEY_DOWN:
+        if k == curses.KEY_DOWN:
             cursor_y = cursor_y + 1
-        elif key == curses.KEY_UP:
+        elif k == curses.KEY_UP:
             cursor_y = cursor_y - 1
-        elif key == curses.KEY_RIGHT:
+        elif k == curses.KEY_RIGHT:
             cursor_x = cursor_x + 1
-        elif key == curses.KEY_LEFT:
+        elif k == curses.KEY_LEFT:
             cursor_x = cursor_x - 1
-        elif key == ord('r'):
-            mct.saveallfiles()
-            bcttimestr = str(mct.gettime())
-            bctdaytimestr = str(mct.getdaytime())
-            bctdaystr = str(mct.getday())
-            bct_time.restart(int(str(mct.gettime())))
-            bct_daytime.restart(int(str(mct.getdaytime())))
 
         cursor_x = max(0, cursor_x)
         cursor_x = min(width-1, cursor_x)
@@ -66,12 +45,10 @@ def main(stdscr):
         # Declaration of strings
         timestr = time.strftime("%H:%M:%S")
         title = "Curses example"[:width-1]
-        subtitle = timestr[:width-1]
-        mctdaytimestr = bct_daytime.total()+" "+bctdaytimestr
-        mcttimestr = bct_time.total()+" "+bcttimestr
-        keystr = "Last key pressed: {}".format(key)[:width-1]
+        subtitle = timestr
+        keystr = "Last key pressed: {}".format(k)[:width-1]
         statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
-        if key == 0:
+        if k == 0:
             keystr = "No key press detected..."[:width-1]
 
         # Centering calculations
@@ -95,16 +72,15 @@ def main(stdscr):
         stdscr.attron(curses.A_BOLD)
 
         # Rendering title
-        stdscr.addstr(start_y, start_x_title, timestr)
+        stdscr.addstr(start_y, start_x_title, title)
 
         # Turning off attributes for title
         stdscr.attroff(curses.color_pair(2))
         stdscr.attroff(curses.A_BOLD)
 
         # Print rest of text
-        stdscr.addstr(start_y + 1, start_x_subtitle, mcttimestr)
-        stdscr.addstr(start_y + 2, start_x_subtitle, mctdaytimestr)
-        stdscr.addstr(start_y + 3, start_x_subtitle, bctdaystr)
+        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
+        stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
         stdscr.addstr(start_y + 5, start_x_keystr, keystr)
         stdscr.move(cursor_y, cursor_x)
 
@@ -112,7 +88,7 @@ def main(stdscr):
         stdscr.refresh()
 
         # Wait for next input
-        key = stdscr.getch()
+        k = stdscr.getch()
 
 
 wrapper(main)
--- bctimer.py
+++ bctimer.py
@@ -5,31 +5,27 @@ class BCTimer():
 
     def __init__(self, start=0):
         self.start=start
+        self.update=self.start
         self.timestart = time()
-        print(self.timestart)
 
     def delta(self):
         return time() - self.timestart        
 
     def printdelta(self):
         sec = timedelta(seconds=int(self.delta()))
-        d = datetime(1,1,1)+sec
+        d = datetime(0,0,0)+sec
         print("%d:%d:%d:%d" % (d.day-1,d.hour, d.minute, d.second))
 
     def printtotal(self):
-        sec = timedelta(seconds=int((self.start/20)+self.delta()))
-        d = datetime(1,1,1)+sec
+        sec = timedelta(seconds=int((self.update/20)+self.delta()))
+        d = datetime(0,0,0)+sec
         print("%d:%d:%d:%d" % (d.day-1,d.hour, d.minute, d.second))
 
+    def total(self):
+        sec = timedelta(seconds=int((self.update/20)+self.delta()))
+        d = datetime(1,1,1)+sec
+        return("%d:%d:%d:%d" % (d.day-1,d.hour, d.minute, d.second))
 
-bct = BCTimer(2073)
-bct.printdelta()
-bct.printtotal()
-print("---")
-sleep(1.0)
-bct.printdelta()
-bct.printtotal()
-print("---")
-sleep(1.0)
-bct.printdelta()
-bct.printtotal()
+    def restart(self,update=0):
+        self.update = update
+        self.timestart = time()
